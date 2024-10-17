1c37fabeef00774ec6d9a5fa1d86337efae1ac0d
faresse <ilyas.faresse@epfl.ch>
2024-10-17 17:19:30 +0200

    Attempt to make the tests pass on the CI

--- app/src/androidTest/java/com/swent/suddenbump/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/swent/suddenbump/ui/authentication/SignInTest.kt
@@ -6,21 +6,35 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
-import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.swent.suddenbump.MainActivity
 import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginTest : TestCase() {
+
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-  @get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+  @get:Rule val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+  @Before
+  public fun setup() {
+    Intents.init() // Initialize intents
+  }
+
+  @After
+  public override fun tearDown() {
+    Intents.release() // Release intents
+  }
 
   @Test
   fun titleAndButtonAreCorrectlyDisplayed() {
