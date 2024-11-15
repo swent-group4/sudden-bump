db6060824ffdb9f058705272738ea873689e46bc
faresse <ilyas.faresse@epfl.ch>
2024-11-15 03:42:50 +0100

    Format to ktfmt

--- app/src/main/java/com/swent/suddenbump/ui/chat/Chat.kt
+++ app/src/main/java/com/swent/suddenbump/ui/chat/Chat.kt
@@ -221,7 +221,9 @@ fun ChatInputBox(viewModel: UserViewModel, otherUser: User?, navigationActions: 
             onClick = {
               if (inputText.text.isNotEmpty()) {
                 val name = otherUser?.firstName + " " + otherUser?.lastName
-                viewModel.sendMessage(inputText.text, viewModel.getCurrentUser().value) // Send message through ViewModel
+                viewModel.sendMessage(
+                    inputText.text,
+                    viewModel.getCurrentUser().value) // Send message through ViewModel
                 inputText = TextFieldValue() // Clear input field after sending
               }
             },
