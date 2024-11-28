28927f13a7f1acd07a515c0ff851b02e3f428780
Noé <noe.zaabi@gmail.com>
2024-11-29 00:43:33 +0100

    KTFMT format

--- app/src/main/java/com/swent/suddenbump/ui/chat/Chat.kt
+++ app/src/main/java/com/swent/suddenbump/ui/chat/Chat.kt
@@ -223,9 +223,9 @@ fun ChatInputBox(viewModel: UserViewModel, otherUser: User?, navigationActions: 
             onClick = {
               if (inputText.text.isNotEmpty()) {
                 val name = otherUser?.firstName + " " + otherUser?.lastName
-                  if (otherUser != null) {
-                      viewModel.sendMessage(inputText.text, user = otherUser)
-                  } // Send message through ViewModel
+                if (otherUser != null) {
+                  viewModel.sendMessage(inputText.text, user = otherUser)
+                } // Send message through ViewModel
                 inputText = TextFieldValue() // Clear input field after sending
               }
             },
