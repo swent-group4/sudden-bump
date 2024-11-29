cf5289e6ccf8534f6edb4a984b64c9cbf1a6ae1b
Deyan Marinov <deyan@marinov.email>
2024-11-29 15:04:20 +0100

    fix ktfmt

--- app/src/androidTest/java/com/swent/suddenbump/ui/utils/ToastMatcher.kt
+++ app/src/androidTest/java/com/swent/suddenbump/ui/utils/ToastMatcher.kt
@@ -9,23 +9,23 @@ import org.hamcrest.TypeSafeMatcher
 
 class ToastMatcher : TypeSafeMatcher<Root?>() {
 
-    override fun describeTo(description: Description?) {
-        description?.appendText("is toast")
-    }
+  override fun describeTo(description: Description?) {
+    description?.appendText("is toast")
+  }
 
-    override fun matchesSafely(item: Root?): Boolean {
-        val type: Int? = item?.windowLayoutParams?.get()?.type
-        Log.d("ToastMatcher", "Type: $type")
-        if (type == WindowManager.LayoutParams.TYPE_TOAST || type == WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY) {
-            val windowToken: IBinder = item.decorView.windowToken
-            val appToken: IBinder = item.decorView.getApplicationWindowToken()
-            if (windowToken === appToken) { // means this window isn't contained by any other windows.
-                Log.d("ToastMatcher", "Toast detected")
-                return true
-            }
-        }
-        Log.d("ToastMatcher", "Not a toast")
-        return false
+  override fun matchesSafely(item: Root?): Boolean {
+    val type: Int? = item?.windowLayoutParams?.get()?.type
+    Log.d("ToastMatcher", "Type: $type")
+    if (type == WindowManager.LayoutParams.TYPE_TOAST ||
+        type == WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY) {
+      val windowToken: IBinder = item.decorView.windowToken
+      val appToken: IBinder = item.decorView.getApplicationWindowToken()
+      if (windowToken === appToken) { // means this window isn't contained by any other windows.
+        Log.d("ToastMatcher", "Toast detected")
+        return true
+      }
     }
-
+    Log.d("ToastMatcher", "Not a toast")
+    return false
+  }
 }
