6a359579200abe37b5e6cc4cda0932430588a485
Dalia <dalia.ghosn@epfl.ch>
2024-11-29 03:13:26 +0100

    Fix ktfmtformat

--- app/src/main/java/com/swent/suddenbump/ui/overview/Settings.kt
+++ app/src/main/java/com/swent/suddenbump/ui/overview/Settings.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.swent.suddenbump.R
