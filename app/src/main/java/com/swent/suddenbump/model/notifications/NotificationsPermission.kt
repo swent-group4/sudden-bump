54d18c6d80716347c384601c927f014ab5053df3
faresse <ilyas.faresse@epfl.ch>
2024-11-29 01:05:22 +0100

    Document isNotificationPermissionGranted

--- app/src/main/java/com/swent/suddenbump/model/notifications/NotificationsPermission.kt
+++ app/src/main/java/com/swent/suddenbump/model/notifications/NotificationsPermission.kt
@@ -9,6 +9,13 @@ import androidx.core.content.ContextCompat
 
 class NotificationsPermission(val activity: Activity) {
 
+  /**
+   * Checks if the notification permission is granted for the application.
+   *
+   * This method requires API level 33 (TIRAMISU) or higher.
+   *
+   * @return `true` if the notification permission is granted, `false` otherwise.
+   */
   @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   fun isNotificationPermissionGranted(): Boolean {
     val notificationPermissionGranted =
