dc28799911eddd8dab894cfcc1c5b0da9f7072bd
Dalia <dalia.ghosn@epfl.ch>
2024-12-18 15:40:56 +0100

    fix import of FirebaseException

--- app/src/test/java/com/swent/suddenbump/model/user/UserRepositoryFirestoreTest.kt
+++ app/src/test/java/com/swent/suddenbump/model/user/UserRepositoryFirestoreTest.kt
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
